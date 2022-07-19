f961da799e4d49afbe22054aec51679b87d11215
Ivan Savu <ivan.savu@gmail.com>
2022-07-19 18:21:30 +0200

    Initial commit

---
+++ A.cs
@@ -0,0 +1,21 @@
+namespace XRefDemo;
+
+/// <summary>
+/// <format type="text/markdown">
+/// <![CDATA[
+/// Info about <xref:A>
+/// > [!NOTE]
+/// > Test note
+/// ]]>
+/// </format>
+/// </summary>
+public class A
+{
+	/// <summary>
+	/// Hello
+	/// </summary>
+	public void Test()
+	{
+
+	}
+}
